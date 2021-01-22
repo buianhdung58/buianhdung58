438706ddb9e398052bb294f6961cbf222cbdf17f
buianhdung58 <anhdungbui2000@gmail.com>
2021-01-22 20:45:26 +0700

    code

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten nguoi dung: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Chao mung " + name + " den voi mon hoc LTQL");
+        }
+    }
+}
---
+++ Ptbac1/Ptbac1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ptbac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Nhap vao gia tri cua a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap vao gia tri cua b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("Phuong trinh co vo so nghiem");
+            }
+            else if (a == 0 && b != 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else
+            {
+                float x = -b * 0.1f / a;
+                Console.WriteLine("Phuong trinh co nghiem x:" + x);
+            }
+        }
+    }
+}
